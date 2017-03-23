888e4385d0e0193df40a65320c13405ef0d58f9c
Leika Konstantin <woollen.kot@gmail.com>
2017-03-23 18:36:51 +0700

    Implemented input, input data

--- src/main.c
+++ src/main.c
@@ -3,6 +3,11 @@
 #include  "deposit.h"
 int main ()
 {
-  
+  unsigned int pod;	// Входные данные pod(срок вклада)
+  unsigned int aod;	// Входные данные aod (период вклада)
+  printf("Введите сумму вклада, т.р. (минимальная сумма 10т.р): ");
+  scanf("%d",&aod);
+  printf("Введите срок вклада(до года, в днях): ");
+  scanf("%d",&pod);
   return 0;
 }
